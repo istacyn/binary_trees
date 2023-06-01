34c748048b91a8db33eb00e58b27a7af3c8a1047
Ivy <ivystacyn@gmail.com>
2023-06-01 11:12:43 -0700

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,6 +23,7 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 			right_height = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
 		}
 		return ((left_height > right_height) ? left_height : right_height);
+	}
 }
 
 /**
